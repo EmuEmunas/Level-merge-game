2dbad001ade6cab8d1b23680b97c9801a74dceb4
EmuEmunas <emuemunas@gmail.com>
2025-07-03 13:56:37 +0200

    bruch3

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('renderCanvas');
+window.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("renderCanvas");
     const engine = new BABYLON.Engine(canvas, true);
-    const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.ArcRotateCamera('camera', Math.PI / 2, Math.PI / 4, 10, BABYLON.Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
-    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 1, 0), scene);
-    const cone = BABYLON.MeshBuilder.CreateCylinder('cone', { diameterTop: 0, diameterBottom: 1, height: 2 }, scene);
 
-    engine.runRenderLoop(() => {
-        scene.render();
-    });
-    window.addEventListener('resize', () => {
-        engine.resize();
-    });
+    const createScene = () => {
+        const scene = new BABYLON.Scene(engine);
+        const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 4, Math.PI / 3, 10, BABYLON.Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+        const cone = BABYLON.MeshBuilder.CreateCylinder("cone", {diameterTop: 0, diameterBottom: 1, height: 2, tessellation: 8}, scene);
+        cone.position.y = 1;
+        return scene;
+    };
+
+    const scene = createScene();
+    engine.runRenderLoop(() => scene.render());
+    window.addEventListener("resize", () => engine.resize());
 });
