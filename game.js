c147ebbd0fad2baa9a874943a56794079fc0df0b
EmuEmunas <emuemunas@gmail.com>
2025-07-03 13:36:01 +0200

    bug fix da index empty

--- game.js
+++ game.js
@@ -1,1 +1,3 @@
-// Beispielhafte JavaScript-Datei
+// game.js (Platzhalter)
+// Hier sollte dein funktionierender game.js Code rein – bitte ersetze diesen Inhalt mit dem funktionierenden Stand.
+console.log("game.js geladen");
